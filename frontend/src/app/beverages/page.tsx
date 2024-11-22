74e10979ec5c48650bc268d4265871591b80a9b0
antonio santos <antoniosantos.devweb@gmail.com>
2024-11-22 08:35:01 -0300

    bugs fixed

--- frontend/src/app/beverages/page.tsx
+++ frontend/src/app/beverages/page.tsx
@@ -1,8 +1,11 @@
+
+
+
 "use client";
 
 import { useEffect, useState } from "react";
 import ProductItem from "../../components/ProductItem";
-import { FaSpinner } from "react-icons/fa";
+import { FaSpinner } from "react-icons/fa"; 
 
 type FoodItem = {
   id: number;
@@ -23,18 +26,24 @@ export default function BeveragesPage() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchFoodItems = async () => {
+    const fetchBeragesItems = async () => {
       try {
         const response = await fetch("https://loja-online-back.onrender.com/api/products/");
-        if (response.ok) {
-          const data: FoodItem[] = await response.json();
+        if (!response.ok) {
+          console.error("Erro ao buscar os itens de Bebidas");
+          return;
+        }
 
-          const filteredItems = data.filter(
-            (item) => item.category === "Bebidas"
-          );
+        const data = await response.json();
+        console.log("Dados retornados pela API:", data);
+
+      
+        const items = Array.isArray(data) ? data : data.products || [];
+        if (Array.isArray(items)) {
+          const filteredItems = items.filter(item => item.category === "Bebidas");
           setFoodItems(filteredItems);
         } else {
-          console.error("Erro ao buscar os itens de bebidas.");
+          console.error("Formato de dados inválido recebido da API.");
         }
       } catch (error) {
         console.error("Erro de rede:", error);
@@ -43,7 +52,7 @@ export default function BeveragesPage() {
       }
     };
 
-    fetchFoodItems();
+    fetchBeragesItems();
   }, []);
 
   if (loading) {
@@ -51,52 +60,42 @@ export default function BeveragesPage() {
       <div className="flex justify-center items-center h-screen bg-opacity-70 bg-gray-100">
         <div className="flex flex-col items-center">
           <FaSpinner className="animate-spin text-blue-600 text-5xl mb-4" />
-          <p className="text-gray-700 font-medium text-lg">
-            Carregando os produtos...
-          </p>
+          <p className="text-gray-700 font-medium text-lg">Carregando os produtos...</p>
         </div>
       </div>
     );
   }
 
   const renderItemsByName = (name: string) => {
-    return foodItems.filter((item) =>
-      item.name.toLowerCase().includes(name.toLowerCase())
-    );
+    return foodItems.filter(item => item.name.toLowerCase().includes(name.toLowerCase()));
   };
 
   return (
     <div className="food-page max-w-6xl mx-auto p-6">
-      <h1 className="text-3xl font-bold text-gray-800 mb-8">Bebidas</h1>
+      <h1 className="text-3xl font-bold text-gray-800 mb-8">Limpeza</h1>
 
       <section className="mb-10">
-        <h2 className="text-2xl font-semibold text-gray-700 mb-4">
-          Refrigerante
-        </h2>
+        <h2 className="text-2xl font-semibold text-gray-700 mb-4">Refrigerantes</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
-          {renderItemsByName("Refrigerante").map((item) => (
+          {renderItemsByName("Refrigerante").map(item => (
             <ProductItem key={item.id} product={item} />
           ))}
         </div>
       </section>
 
       <section className="mb-10">
-        <h2 className="text-2xl font-semibold text-gray-700 mb-4">
-          Bebidas alcolicas
-        </h2>
+        <h2 className="text-2xl font-semibold text-gray-700 mb-4"> Bebidas alcolicas</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
-          {renderItemsByName("Vodka").map((item) => (
+          {renderItemsByName("Vodka").map(item => (
             <ProductItem key={item.id} product={item} />
           ))}
         </div>
       </section>
 
       <section className="mb-10">
-        <h2 className="text-2xl font-semibold text-gray-700 mb-4">
-          Energeticos
-        </h2>
+        <h2 className="text-2xl font-semibold text-gray-700 mb-4">Energeticos</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mb-6">
-          {renderItemsByName("Energetico").map((item) => (
+          {renderItemsByName("Energetico").map(item => (
             <ProductItem key={item.id} product={item} />
           ))}
         </div>
